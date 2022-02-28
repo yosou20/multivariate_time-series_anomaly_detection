6291f9d85b08a0bc8c4df783f0c53a29dd42eb1b
yosou20 <yosou20@student.sdu.dk>
2022-02-28 10:49:23 +0100

    environment setup for Master Project

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
-print ("Hello World 2")
+def print_sum(a,b):
+    """add function
+
+    Args:
+        a (int): a number
+        b (int): a number
+
+    Returns:
+        int:  sum (a,a)
+    """
+    return a + b
+
+myprint = print_sum(4,6)
+print(myprint)
+
